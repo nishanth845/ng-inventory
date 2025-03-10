427bd3e7c17b5b98d7eb6bce37e701091ebea416
nishanth845 <82376753+nishanth845@users.noreply.github.com>
2025-03-10 23:26:23 +0530

    amend

--- inventory/src/app/app.routes.ts
+++ inventory/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',redirectTo:'landingPage',pathMatch: 'full'},
+  {path: 'landingPage',component :LandingPageComponent}
+];
